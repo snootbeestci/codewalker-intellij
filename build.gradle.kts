115cb7c9bf24eac3adb0c266ae83e2ef3328cb86
snootbeestci <33792634+snootbeestci@users.noreply.github.com>
2026-04-28 13:00:00 +0100

    Update build.gradle.kts
    Bump proto version

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.snootbeestci:codewalker-proto:v0.1.0") {
+    implementation("com.github.snootbeestci:codewalker-proto:v0.2.0") {
         exclude(group = "org.jetbrains.kotlinx", module = "kotlinx-coroutines-core")
         exclude(group = "org.jetbrains.kotlinx", module = "kotlinx-coroutines-core-jvm")
     }
